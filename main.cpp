09676aea7dffaa106b68d3cd0941c8ed34dafb65
Denis N <nov_den@mail.ru>
2020-11-16 22:03:20 +0300

    выровнял

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 // Поменять местами максимальный и последний отрицательный элементы массива M(40).
 
-
 #include <iostream>
 #include <conio.h>
 #include <stdlib.h>
@@ -11,29 +10,30 @@ using namespace std;
 int main()
 {
 constexpr int size = 20;
-    int mass[size]={5, 4 ,3, 18, 7, -6, 3};
+    int mass[size] = {5, 4 ,3, 18, 7, -6, 3};
 
     int i;
     int max = mass[0];
-    int index=0;
+    int index = 0;
 
-    for(int i=0; i<size; ++i)
+    for( int i = 0 ; i < size ; ++i )
     {
      printf("%d ", mass[i]);
     }
         printf("\n");
 
-   for (int i = 0; i < size; ++i)       //ищем максимальное число
+   for ( int i = 0 ; i < size ; ++i )     //ищем максимальное число
     {
-      if (mass[i] > max)                //прогоняем цикл, сравнивая все значения с максимальным, обновляяе его
+      if ( mass[i] > max )               //прогоняем цикл, сравнивая все значения с максимальным, обновляяе его
       {
-        max = mass[i];                  // максимальное значение
-        index = i;                      // записали индекс максимального числа
+        max = mass[i];                   // максимальное значение
+        index = i;                       // записали индекс максимального числа
       }
     }
-    for (i = size - 1; i > 0; i--)   //начинаем с конца искать первый отриц. элемент
+
+    for ( i = size - 1 ; i > 0 ; i-- )   //начинаем с конца искать первый отриц. элемент
     {
-      if ( mass[i] < 0)
+      if ( mass[i] < 0 )
       {
         int temp = mass[i];
         mass[i] = mass[index];
@@ -41,7 +41,8 @@ constexpr int size = 20;
             break;
       }
     }
-    for ( i = 0; i <size; i++)
+
+    for ( i = 0 ; i <size ; i++ )
     {
       printf("%2d ", mass[i] );
     }
